a0e1e61cf55a88aacef4f49077dd1637cb9c4db9
Chirayu Nikam <100028643+Chirayunikam@users.noreply.github.com>
2024-06-30 18:25:20 +0530

    Update LoadingMessage.jsx

--- Frontend/src/components/LoadingMessage/LoadingMessage.jsx
+++ Frontend/src/components/LoadingMessage/LoadingMessage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import styles from "./LoadingMessage.module.css";
 import hourGlass from "../../assets/gif/Hourglass.gif";
 function LoadingMessage() {
