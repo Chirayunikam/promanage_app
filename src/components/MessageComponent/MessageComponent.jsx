7ca75590ed65f32e46852234efe720dce63b9144
Chirayu Nikam <100028643+Chirayunikam@users.noreply.github.com>
2024-06-30 17:58:25 +0530

    Delete src directory

--- src/components/MessageComponent/MessageComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "./MessageComponent.module.css";
-function MessageComponent({ message }) {
-  return <div className={styles.messageContainer}>{message}</div>;
-}
-
-export default MessageComponent;
