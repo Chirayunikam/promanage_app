52b4e0e4b5ce903ae15c479e3f2d1bef7d3f4277
Chirayu Nikam <100028643+Chirayunikam@users.noreply.github.com>
2024-06-30 18:33:14 +0530

    Update RegisterPage.jsx

--- Frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ Frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Register from "../../components/Register/Register";
 import CoverImage from "../../components/CoverImage/CoverImage";
-
+ 
 function RegisterPage() {
   return (
     <div style={{ display: "flex", height: "100vh" }}>
